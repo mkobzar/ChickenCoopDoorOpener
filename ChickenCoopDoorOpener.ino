9cec5820b4cdee7be9103118917b1df6f9d52cea
mike ko <kobzar@outlook.com>
2018-02-17 22:53:59 -0600

    update...

--- ChickenCoopDoorOpener.ino
+++ ChickenCoopDoorOpener.ino
@@ -1,23 +1,20 @@
 /*
-in use
-L298N Dual H-Bridge Motor Controller to open/close the door
-relay (to save battery power) connecter to pin 1
-Pro Mini 328 3.3v 8MHz
+  in use
+  L298N Dual H-Bridge Motor Controller to open/close the door
+  relay (to save battery power) connecter to pin 1
+  Pro Mini 328 3.3v 8MHz
 
-Silicon Labs CP210x USB to UART Bridge pinout <-|-> Pro Mini RobotDyn 328 3.3v 8MHz
-DRT - DTR
-RXD - TXO
-TXD - RXI
-+3.3V - VCC
-GND - GRN
+  Silicon Labs CP210x USB to UART Bridge pinout <-|-> Pro Mini RobotDyn 328 3.3v 8MHz
+  DRT - DTR
+  RXD - TXO
+  TXD - RXI
+  +3.3V - VCC
+  GND - GRN
 */
 
-
-// #define DEBUG
-
-#include <JeeLib.h>  
+#include <JeeLib.h>
 ISR(WDT_vect) {
-	Sleepy::watchdogEvent();
+  Sleepy::watchdogEvent();
 }
 
 
@@ -32,111 +29,85 @@ int relay = 13;
 int maxDoorMove = 30000;
 
 void setup() {
-#ifdef DEBUG
-	delay(3000);
-	Serial.begin(9600);
-	Serial.println("Serial.begin");
-#endif
-	pinMode(motor1a_pin, OUTPUT);
-	pinMode(motor1b_pin, OUTPUT);
-	pinMode(motor1speed_pin, OUTPUT);
-	pinMode(senseClosed_pin, INPUT);
-	pinMode(senseOpened_pin, INPUT);
-	pinMode(relay, OUTPUT);
+  pinMode(motor1a_pin, OUTPUT);
+  pinMode(motor1b_pin, OUTPUT);
+  pinMode(motor1speed_pin, OUTPUT);
+  pinMode(senseClosed_pin, INPUT);
+  pinMode(senseOpened_pin, INPUT);
+  pinMode(relay, OUTPUT);
 }
 
 void loop() {
-	bool isDark = IsDark();
-#ifdef DEBUG
-	Serial.println("loop begin");
-	Serial.print("doorState=");
-	Serial.print(doorState);
-	Serial.print(", isDark=");
-	Serial.println(isDark);
-#endif
-	if (isDark && doorState != -1) {
-		door(false); // close
-#ifdef DEBUG
-		Serial.println("closed");
-#endif
-	}
-	else if (!isDark && doorState != 1) {
-		door(true); // open
-#ifdef DEBUG
-		Serial.println("opened");
-#endif
-	}
-#ifdef DEBUG
-	Serial.println("deep sleep");
-#endif
-	Sleepy::loseSomeTime(120000); // next check is 2 minutes later
+  bool isDark = IsDark();
+  if (isDark && doorState != -1) {
+    door(false); // close
+  }
+  else if (!isDark && doorState != 1) {
+    door(true); // open
+  }
+  Sleepy::loseSomeTime(900000); // next check is 15 minutes later
 }
 
 
 bool IsDark()
 {
-	// light < 100: door should be closed
-	// light > 200: door should be opened
-	int light = analogRead(light_pin);
-	int threshold = 100 + -40 * doorState;
-#ifdef DEBUG
-	Serial.print(light);
-#endif
-	if (light < threshold) {
-#ifdef DEBUG
-		Serial.println(" = night");
-#endif
-		return true;
-	}
-	else {
-#ifdef DEBUG
-		Serial.println(" = day");
-#endif
-		return false;
-	}
+ int light = 0;
+ int test1 = analogRead(light_pin);
+  delay(100);
+ int test2 = analogRead(light_pin);
+
+  if (test1 < 25 && test2 < 25) {
+    light = 0;
+  }
+  else if (test1 > 145 && test2 > 145) {
+    light = 200;
+  }
+  else {
+    light = (test1 + test2) / 2;
+  }
+
+  // light < 25: night
+  // light > 145: day
+  // int light =  analogRead(light_pin);
+  int threshold = 85 + -60 * doorState;
+  if (light < threshold) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 
 void door(bool open)
 {
-	if (!open) {
-		// when door should be closed - delay for 30 minutes
-		Sleepy::loseSomeTime(1800000); // 30 * 60 * 1000 
-	}
+  // give power to motor driver
+  digitalWrite(relay, 1);
 
-	// give power to motor driver
-	digitalWrite(relay, 1);
+  //reset_millis
+  extern volatile unsigned long timer0_millis, timer0_overflow_count;
+  noInterrupts();
+  timer0_millis = timer0_overflow_count = 0;
+  interrupts();
 
-	//reset_millis
-	extern volatile unsigned long timer0_millis, timer0_overflow_count;
-	noInterrupts();
-	timer0_millis = timer0_overflow_count = 0;
-	interrupts();
+  int sencePin = open ? senseOpened_pin : senseClosed_pin;
+  analogWrite(motor1speed_pin, 1200);
+  digitalWrite(motor1a_pin, !open);
+  digitalWrite(motor1b_pin, open);
 
-	int sencePin = open ? senseOpened_pin : senseClosed_pin;
-#ifdef DEBUG
-	Serial.println("DoorMove()");
-#endif
-	analogWrite(motor1speed_pin, 1200);
-	digitalWrite(motor1a_pin, !open);
-	digitalWrite(motor1b_pin, open);
+  while (digitalRead(sencePin) == LOW && millis() < maxDoorMove) {
+    delay(20);
+  }
 
-	while (digitalRead(sencePin) == LOW && millis() < maxDoorMove) {
-		delay(20);
-	}
+  // let run door closing another 2 sec after door sensor alreary reported "door already closed"
+  if (!open) {
+    delay(2000);
+  }
 
-	// let move door closing another 1 sec after door sensot alreary reported "door already closed"
-	if (!open) {
-		delay(2000);
-	}
-
-	//DoorStop
-#ifdef DEBUG
-	Serial.println("DoorStop()");
-#endif
-	analogWrite(motor1speed_pin, 0);
-	digitalWrite(motor1a_pin, LOW);
-	digitalWrite(motor1b_pin, LOW);
-	digitalWrite(relay, 0);
-	doorState = open ? 1 : -1;
+  //DoorStop
+  analogWrite(motor1speed_pin, 0);
+  digitalWrite(motor1a_pin, LOW);
+  digitalWrite(motor1b_pin, LOW);
+  digitalWrite(relay, 0);
+  doorState = open ? 1 : -1;
 }
